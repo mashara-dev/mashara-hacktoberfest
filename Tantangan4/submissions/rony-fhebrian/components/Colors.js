f1a7bf7fb29845a26fa9757635bfb12543771c35
Rony Fhebrian <rfhebrian@gmail.com>
2019-10-18 19:38:52 +0700

    fix: Change readme.md and give example in submissions folder

---
+++ Tantangan4/submissions/rony-fhebrian/components/Colors.js
@@ -0,0 +1,23 @@
+export default {
+    // BACKGROUND COLOR
+    white: '#fff',
+    background_navy: '#183374',
+    light_red: '#ff4a81',
+    border_gray: '#d4d4d4',
+    background_gray: '#f1f1f1',
+    background_red: '#dc136c',
+    background_verified: '#f1c208',
+    separator_gray: '#f2f2f2',
+
+    // TEXT COLOR
+    black_text: '#1a1a1a',
+    gray_text: '#707070',
+    red_text: '#dc136c',
+    widget_time_color: '#f6ff00',
+    blue_text: '#5c7efb',
+
+    // MISC
+    success_toast: '#00c325',
+    error_toast: '#fd7f7f',
+    disabled_toast: '#bebebe'
+}
